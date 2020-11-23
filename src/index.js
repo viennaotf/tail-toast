a2deb12c92f4942a6dfc0b01dfe9a2c6c7aee5ab
Ferhat Yildirim <s1117804@student.windesheim.nl>
2020-11-23 19:04:52 +0100

    attempt #5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import TailToast from './TailToast.vue'
 import { spawn, removeElement } from './utils.js'
 
+import options from './options'
+
 let installed = false
 
 const containerClasses = [
@@ -11,7 +13,34 @@ const containerClasses = [
 export default {
   install(Vue) {
     if (installed) return
-    Vue.component('TailToast', TailToast)
+
+    const toasts = document.createElement('div')
+    containerClasses.forEach(c => toasts.classList.add(c))
+    if (options.defaults && options.defaults.containerClasses) {
+      toasts.classList.add(options.defaults.containerClasses)
+    }
+    toasts.setAttribute('id', 'toasts')
+    document.body.appendChild(toasts)
+
+    const ToastProgrammatic = {
+      show (props) {
+        if (typeof props === 'string') props = { message: props }
+        return spawn('toasts', props, TailToast, Vue, options)
+      },
+      success (props) {
+        return spawn('toasts', {type: 'success', message: props}, TailToast, Vue, options)
+      },
+      info (props) {
+        return spawn('toasts', {type: 'info', message: props}, TailToast, Vue, options)
+      },
+      danger (props) {
+        return spawn('toasts', {type: 'danger', message: props}, TailToast, Vue, options)
+      },
+      warning (props) {
+        return spawn('toasts', {type: 'warning', message: props}, TailToast, Vue, options)
+      },
+    }
+    Vue.toast = Vue.prototype.$toast = ToastProgrammatic
     installed = true
   }
 }
