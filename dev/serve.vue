db1234b5ad875b9c6627294e6ccce6dcda6c5cdf
Ferhat Yildirim <s1117804@student.windesheim.nl>
2020-11-23 18:31:32 +0100

    initial

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import TailToast from '@/TailToast.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TailToast
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <TailToast />
+  </div>
+</template>
