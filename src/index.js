273d18492414db6d41bea18df894dcca8941feb5
Ferhat Yildirim <s1117804@student.windesheim.nl>
2020-11-23 18:51:11 +0100

    attempt #4

--- src/index.js
+++ src/index.js
@@ -11,19 +11,7 @@ const containerClasses = [
 export default {
   install(Vue) {
     if (installed) return
-    const CONSTRUCTOR = Vue.extend(TailToast)
-    const CACHE = {}
-
-    function toast(msg, options = {}) {
-      options.message = msg
-      let toast = CACHE[options.id] || (CACHE[options.id] = new CONSTRUCTOR)
-      if (!toast.$el) {
-        let vm = toast.$mount()
-        document.querySelector(options.parent || 'body').appendChild(vm.$el)
-      }
-      toast.queue.push(options)
-    }
-    Vue.toast = Vue.prototype.$toast = toast
+    Vue.component('TailToast', TailToast)
     installed = true
   }
 }
